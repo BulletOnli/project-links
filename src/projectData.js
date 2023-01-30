1082934b1c911f668d3b8c6b52f4853b553c98f0
Gemmuel <gemmueldelacruz@gmail.com>
2023-01-30 23:38:47 +0800

    added new links

--- src/projectData.js
+++ src/projectData.js
@@ -21,11 +21,25 @@ const projectData = [
         id: 3,
     },
     {
+        title: 'Fermented Rice',
+        description: "Food website I created for my Client (my first client project)",
+        img: require('../src/images/fermentedrice.png'),
+        url: 'https://iceflix.netlify.app/',
+        id: 4,
+    },
+    {
         title: 'Message Generator',
         description: 'A message generator that generates random sweet messages.',
         img: require('../src/images/msggenerator.png'),
         url: 'https://sweetmsg-generator.netlify.app/',
-        id: 4,
+        id: 5,
+    },
+    {
+        title: 'Bean Block',
+        description: 'A coffee website. Also the first website I created.',
+        img: require('../src/images/beanblock.png'),
+        url: 'https://beanblock.netlify.app/',
+        id: 6,
     }
 ]
 
