9ed2ad56d7e00a67aa721cd02bda3ccc1ae50a3b
Gemmuel <gemmueldelacruz@gmail.com>
2023-02-05 23:12:16 +0800

    removed the url in fermented rice

--- src/projectData.js
+++ src/projectData.js
@@ -1,46 +1,50 @@
 const projectData = [
     {
-        title: 'Weather App',
-        description: 'A real-time weather app. You can look up your country, city, province, etc.',
-        img: require('../src/images/weatherapp.png'),
-        url: 'https://klimangayon.netlify.app/',
+        title: "Weather App",
+        description:
+            "A real-time weather app. You can look up your country, city, province, etc.",
+        img: require("../src/images/weatherapp.png"),
+        url: "https://klimangayon.netlify.app/",
         id: 1,
     },
     {
-        title: 'TypeMoko',
-        description: 'A typing website where you can test and improve your typing skills.',
-        img: require('../src/images/typemoko.png'),
-        url: 'https://typemoko.netlify.app/',
+        title: "TypeMoko",
+        description:
+            "A typing website where you can test and improve your typing skills.",
+        img: require("../src/images/typemoko.png"),
+        url: "https://typemoko.netlify.app/",
         id: 2,
     },
     {
-        title: 'IceFlix',
+        title: "IceFlix",
         description: "A movie app where you cant even watch a movie",
-        img: require('../src/images/iceflix.png'),
-        url: 'https://iceflix.netlify.app/',
+        img: require("../src/images/iceflix.png"),
+        url: "https://iceflix.netlify.app/",
         id: 3,
     },
     {
-        title: 'Fermented Rice',
-        description: "Food website I created for my Client (my first client project)",
-        img: require('../src/images/fermentedrice.png'),
-        url: 'https://iceflix.netlify.app/',
+        title: "Fermented Rice",
+        description:
+            "Food website I created for my Client (my first client project)",
+        img: require("../src/images/fermentedrice.png"),
+        url: "#",
         id: 4,
     },
     {
-        title: 'Message Generator',
-        description: 'A message generator that generates random sweet messages.',
-        img: require('../src/images/msggenerator.png'),
-        url: 'https://sweetmsg-generator.netlify.app/',
+        title: "Message Generator",
+        description:
+            "A message generator that generates random sweet messages.",
+        img: require("../src/images/msggenerator.png"),
+        url: "https://sweetmsg-generator.netlify.app/",
         id: 5,
     },
     {
-        title: 'Bean Block',
-        description: 'A coffee website. Also the first website I created.',
-        img: require('../src/images/beanblock.png'),
-        url: 'https://beanblock.netlify.app/',
+        title: "Bean Block",
+        description: "A coffee website. Also the first website I created.",
+        img: require("../src/images/beanblock.png"),
+        url: "https://beanblock.netlify.app/",
         id: 6,
-    }
-]
+    },
+];
 
-export default projectData
+export default projectData;
