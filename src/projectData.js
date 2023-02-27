096f248bcee096da50c55b39549df13f28dd3d2f
Gemmuel <gemmueldelacruz@gmail.com>
2023-02-27 21:01:57 +0800

    added new project

--- src/projectData.js
+++ src/projectData.js
@@ -1,11 +1,20 @@
+import { nanoid } from "nanoid";
+
 const projectData = [
+    {
+        title: "CookTopia",
+        description: "A dynamic recipe app made with React",
+        img: require("../src/images/cooktopia.png"),
+        url: "https://cooktopias.netlify.app/",
+        id: nanoid(),
+    },
     {
         title: "Weather App",
         description:
             "A real-time weather app. You can look up your country, city, province, etc.",
         img: require("../src/images/weatherapp.png"),
         url: "https://klimangayon.netlify.app/",
-        id: 1,
+        id: nanoid(),
     },
     {
         title: "TypeMoko",
@@ -13,14 +22,14 @@ const projectData = [
             "A typing website where you can test and improve your typing skills.",
         img: require("../src/images/typemoko.png"),
         url: "https://typemoko.netlify.app/",
-        id: 2,
+        id: nanoid(),
     },
     {
         title: "IceFlix",
         description: "A movie app where you cant even watch a movie",
         img: require("../src/images/iceflix.png"),
         url: "https://iceflix.netlify.app/",
-        id: 3,
+        id: nanoid(),
     },
     {
         title: "Fermented Rice",
@@ -28,7 +37,7 @@ const projectData = [
             "Food website I created for my Client (my first client project)",
         img: require("../src/images/fermentedrice.png"),
         url: "#",
-        id: 4,
+        id: nanoid(),
     },
     {
         title: "Message Generator",
@@ -36,14 +45,14 @@ const projectData = [
             "A message generator that generates random sweet messages.",
         img: require("../src/images/msggenerator.png"),
         url: "https://sweetmsg-generator.netlify.app/",
-        id: 5,
+        id: nanoid(),
     },
     {
         title: "Bean Block",
         description: "A coffee website. Also the first website I created.",
         img: require("../src/images/beanblock.png"),
         url: "https://beanblock.netlify.app/",
-        id: 6,
+        id: nanoid(),
     },
 ];
 
